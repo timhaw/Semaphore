a0b7fa09d5d7b453be8919a9e713bada0ced7878
timhaw <tim@intercress.com>
2019-11-02 19:44:52 +0000

    Update Semaphore.groovy

--- src/com/intercress/Semaphore.groovy
+++ src/com/intercress/Semaphore.groovy
@@ -11,13 +11,13 @@ class Semaphore {
        
        def credentials = JsonOutput.toJson([auth: 'admin', password: 'admin'])    
        
-       def cookie = httpRequest \
+       def cookie = httpRequest( \
            acceptType: 'APPLICATION_JSON', \
            consoleLogResponseBody: true, \
            contentType: 'APPLICATION_JSON', \
            httpMode: 'POST', \
            requestBody: credentials, \
-           url: "http://localhost:3000/api/auth/login"
+           url: "http://localhost:3000/api/auth/login")
 
 //        def cookieContent = cookie.headers.get("Set-Cookie")
        
