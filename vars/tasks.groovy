b8647d7f566a21d1df15365f0b5372bb57eb80df
timhaw <tim@intercress.com>
2019-11-02 15:50:13 +0000

    Update tasks.groovy

--- vars/tasks.groovy
+++ vars/tasks.groovy
@@ -1,10 +1,10 @@
 import com.intercress.*
 
-def call(String) {
-  echo "Hello, ${GlobalVars.foo}"
+def call(String playbook) {
+  echo "Hello, ${playbook}"
+//  echo "Hello, ${GlobalVars.foo}"
 }
 
 //def call(String playbook = 'local.yml') {
-//    def newString = new Semaphore().Semapi(playbook)
 //    echo ${playbook}
 //}
