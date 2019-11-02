11861363d4cfa9a1c7b8f3f72776149215cc26da
timhaw <tim@intercress.com>
2019-11-02 22:45:57 +0000

    Update Semaphore.groovy

--- src/com/intercress/Semaphore.groovy
+++ src/com/intercress/Semaphore.groovy
@@ -6,7 +6,7 @@ class Semaphore {
        
     static String foo = 'bar'
        
-    def myMethod() {
+    static def myMethod() {
         httpRequest acceptType: 'APPLICATION_JSON', consoleLogResponseBody: true, contentType: 'APPLICATION_JSON', httpMode: 'POST', url: "http://localhost:3000/api/auth/login"
     }
            
