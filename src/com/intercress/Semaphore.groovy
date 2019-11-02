cd091f77cbdc1fb2fd8309bb1b592f66f1613e62
timhaw <tim@intercress.com>
2019-11-02 18:30:41 +0000

    Update Semaphore.groovy

--- src/com/intercress/Semaphore.groovy
+++ src/com/intercress/Semaphore.groovy
@@ -8,6 +8,13 @@ class Semaphore {
    
     static void Semapi(String[] args) {
        def credentials = JsonOutput.toJson([auth: 'admin', password: 'admin'])    
+       def cookie = httpRequest \
+           acceptType: 'APPLICATION_JSON', \
+           consoleLogResponseBody: true, \
+           contentType: 'APPLICATION_JSON', \
+           httpMode: 'POST', \
+           requestBody: credentials, \
+           url: "http://localhost:3000/api/auth/login"
     }
     
    //    def schema = JsonOutput.toJson([template_id: 1, debug: false, dry_run: false, playbook: playbook, environment: ''])   
