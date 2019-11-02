fe2297960cc83215d3b7288e956cb9c13733640b
timhaw <tim@intercress.com>
2019-11-02 14:51:56 +0000

    Update Semaphore.groovy

--- src/com/intercress/Semaphore.groovy
+++ src/com/intercress/Semaphore.groovy
@@ -1,5 +1,5 @@
 #!/usr/bin/env groovy
-package com.intercress.Semaphore
+package com.intercress
 import groovy.json.JsonOutput
 
 class Semaphore {
@@ -7,24 +7,26 @@ class Semaphore {
 
         def credentials = JsonOutput.toJson([auth: 'admin', password: 'admin'])    
         def schema = JsonOutput.toJson([template_id: 1, debug: false, dry_run: false, playbook: playbook, environment: ''])
+        
+        println(playbook)
 
-        def cookie = httpRequest( \
-            acceptType: 'APPLICATION_JSON', \
-            consoleLogResponseBody: true, \
-            contentType: 'APPLICATION_JSON', \
-            httpMode: 'POST', \
-            requestBody: credentials, \
-            url: "http://localhost:3000/api/auth/login")
+//        def cookie = httpRequest( \
+//            acceptType: 'APPLICATION_JSON', \
+//            consoleLogResponseBody: true, \
+//            contentType: 'APPLICATION_JSON', \
+//            httpMode: 'POST', \
+//            requestBody: credentials, \
+//            url: "http://localhost:3000/api/auth/login")
 
-        def cookieContent = cookie.headers.get("Set-Cookie")    
+//        def cookieContent = cookie.headers.get("Set-Cookie")    
 
-        httpRequest( \
-            acceptType: 'APPLICATION_JSON', \
-            consoleLogResponseBody: true, \
-            contentType: 'APPLICATION_JSON', \
-            customHeaders: [[name: 'Cookie', value: cookieContent[0]]], \
-            httpMode: 'POST', \
-            requestBody: schema, \
-            url: "http://localhost:3000/api/project/1/tasks")
-    }
+//        httpRequest( \
+//            acceptType: 'APPLICATION_JSON', \
+//            consoleLogResponseBody: true, \
+//            contentType: 'APPLICATION_JSON', \
+//            customHeaders: [[name: 'Cookie', value: cookieContent[0]]], \
+//            httpMode: 'POST', \
+//            requestBody: schema, \
+//            url: "http://localhost:3000/api/project/1/tasks")
+//    }
 }
