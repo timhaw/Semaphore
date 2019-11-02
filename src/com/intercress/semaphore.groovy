bb5b834fbe87e3b03ba2f8baac63f922b5bacf32
timhaw <tim@intercress.com>
2019-11-02 12:28:37 +0000

    Create semaphore.groovy

---
+++ src/com/intercress/semaphore.groovy
@@ -0,0 +1,29 @@
+package com.intercress.semaphore
+import groovy.json.JsonOutput
+
+class semaphore {
+    def semapi(playbook) {
+
+        def credentials = JsonOutput.toJson([auth: 'admin', password: 'admin'])    
+        def schema = JsonOutput.toJson([template_id: 1, debug: false, dry_run: false, playbook: playbook, environment: ''])
+
+        def cookie = httpRequest \
+            acceptType: 'APPLICATION_JSON', \
+            consoleLogResponseBody: true, \
+            contentType: 'APPLICATION_JSON', \
+            httpMode: 'POST', \
+            requestBody: credentials, \
+            url: "http://localhost:3000/api/auth/login"
+
+        def cookieContent = cookie.headers.get("Set-Cookie")    
+
+        httpRequest \
+            acceptType: 'APPLICATION_JSON', \
+            consoleLogResponseBody: true, \
+            contentType: 'APPLICATION_JSON', \
+            customHeaders: [[name: 'Cookie', value: cookieContent[0]]], \
+            httpMode: 'POST', \
+            requestBody: schema, \
+            url: "http://localhost:3000/api/project/1/tasks"
+    }
+}
