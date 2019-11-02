c3377779f529410ad63aff3164d9ec40019a6c2c
timhaw <tim@intercress.com>
2019-11-02 22:44:30 +0000

    Update Semaphore.groovy

--- src/com/intercress/Semaphore.groovy
+++ src/com/intercress/Semaphore.groovy
@@ -2,14 +2,14 @@
 package com.intercress
 import groovy.json.JsonOutput
 
-    def myMethod() {
-        httpRequest acceptType: 'APPLICATION_JSON', consoleLogResponseBody: true, contentType: 'APPLICATION_JSON', httpMode: 'POST', url: "http://localhost:3000/api/auth/login"
-    }
-           
 class Semaphore {
        
     static String foo = 'bar'
        
+    def myMethod() {
+        httpRequest acceptType: 'APPLICATION_JSON', consoleLogResponseBody: true, contentType: 'APPLICATION_JSON', httpMode: 'POST', url: "http://localhost:3000/api/auth/login"
+    }
+           
     static String Semapi(String[] password) {
            def credentials = JsonOutput.toJson([auth: 'admin', password: 'admin'])
            
