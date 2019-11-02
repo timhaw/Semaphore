7118645201175ac1b85bacb94cc6a29907ab746c
timhaw <tim@intercress.com>
2019-11-02 17:36:12 +0000

    Update Semaphore.groovy

--- src/com/intercress/Semaphore.groovy
+++ src/com/intercress/Semaphore.groovy
@@ -4,10 +4,10 @@ package com.intercress
 import groovy.json.JsonOutput
 
 class Semaphore {
-    Semapi(String playbook) {
+    def Semapi(String playbook) {
    
    //    def schema = JsonOutput.toJson([template_id: 1, debug: false, dry_run: false, playbook: playbook, environment: ''])   
-       static String foo = ${playbook}
+   //    static String foo = ${playbook}
     }
    // refer to this in a pipeline using:
    //
