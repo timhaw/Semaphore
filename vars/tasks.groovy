d77acc866569589543e429881e4f19bf62d6c87c
timhaw <tim@intercress.com>
2019-11-02 23:44:45 +0000

    Update tasks.groovy

--- vars/tasks.groovy
+++ vars/tasks.groovy
@@ -1,19 +1,20 @@
 import com.intercress.*
 import groovy.json.JsonOutput
 
-def myMethod() {
+def String myMethod() {
     def credentials = JsonOutput.toJson([auth: 'admin', password: 'admin'])
-    httpRequest acceptType: 'APPLICATION_JSON', consoleLogResponseBody: true, contentType: 'APPLICATION_JSON', httpMode: 'POST', requestBody: credentials, url: "http://localhost:3000/api/auth/login"
+    def cookie = httpRequest acceptType: 'APPLICATION_JSON', consoleLogResponseBody: true, contentType: 'APPLICATION_JSON', httpMode: 'POST', requestBody: credentials, url: "http://localhost:3000/api/auth/login"
+    return cookie
 }
   
 def call(String playbook) {
   
-  myMethod()
+  retval = myMethod()
   
   //  def wibble = Semaphore.foo
   def wibble = new Semaphore()
   
-  retval = wibble.Semapi(playbook)
+  // retval = wibble.Semapi(playbook)
   
   echo "Hello, ${retval}"
 }
