26193e22ec916dd29b29ebbc0204933c506dc90a
timhaw <tim@intercress.com>
2019-11-02 23:33:17 +0000

    Update tasks.groovy

--- vars/tasks.groovy
+++ vars/tasks.groovy
@@ -2,7 +2,13 @@ import com.intercress.*
 
 def call(String playbook) {
   
-//  def wibble = Semaphore.foo
+    static def myMethod() {
+        httpRequest acceptType: 'APPLICATION_JSON', consoleLogResponseBody: true, contentType: 'APPLICATION_JSON', httpMode: 'POST', url: "http://localhost:3000/api/auth/login"
+    }
+  
+  myMethod()
+  
+  //  def wibble = Semaphore.foo
   def wibble = new Semaphore()
   
   retval = wibble.Semapi(playbook)
