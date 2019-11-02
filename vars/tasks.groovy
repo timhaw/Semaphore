e696e44baf16954e01b6dd61ec354d1d632bf959
timhaw <tim@intercress.com>
2019-11-02 23:51:32 +0000

    Update tasks.groovy

--- vars/tasks.groovy
+++ vars/tasks.groovy
@@ -3,8 +3,9 @@ import groovy.json.JsonOutput
 
 String myMethod() {
     def credentials = JsonOutput.toJson([auth: 'admin', password: 'admin'])
-    def String cookie = httpRequest acceptType: 'APPLICATION_JSON', consoleLogResponseBody: true, contentType: 'APPLICATION_JSON', httpMode: 'POST', requestBody: credentials, url: "http://localhost:3000/api/auth/login"
-    return cookie
+    def cookie = httpRequest acceptType: 'APPLICATION_JSON', consoleLogResponseBody: true, contentType: 'APPLICATION_JSON', httpMode: 'POST', requestBody: credentials, url: "http://localhost:3000/api/auth/login"
+    def cookieContent = cookie.headers.get("Set-Cookie")    
+    return cookieContent
 }
   
 def call(String playbook) {
