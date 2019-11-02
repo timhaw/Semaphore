1df0975699961b0bb439012dfa1df2ecabf233cf
timhaw <tim@intercress.com>
2019-11-02 23:35:31 +0000

    Update tasks.groovy

--- vars/tasks.groovy
+++ vars/tasks.groovy
@@ -1,6 +1,6 @@
 import com.intercress.*
 
-    static def myMethod() {
+    def myMethod() {
         httpRequest acceptType: 'APPLICATION_JSON', consoleLogResponseBody: true, contentType: 'APPLICATION_JSON', httpMode: 'POST', url: "http://localhost:3000/api/auth/login"
     }
   
