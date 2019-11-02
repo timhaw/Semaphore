186ca6e17521e0e572788c887c8eb9a756e2eaab
timhaw <tim@intercress.com>
2019-11-02 15:37:45 +0000

    Update tasks.groovy

--- vars/tasks.groovy
+++ vars/tasks.groovy
@@ -1,7 +1,8 @@
 import com.intercress
 
 def call(String name = 'human') {
-  echo "Hello, ${name}."
+  echo "Hello, ${GlobalVars.foo}."
+   // println GlobalVars.foo
 }
 
 //def call(String playbook = 'local.yml') {
