5d0569e49566fa7bda0c5e25289425690a2f0e40
timhaw <tim@intercress.com>
2019-11-02 22:43:27 +0000

    Update Semaphore.groovy

--- src/com/intercress/Semaphore.groovy
+++ src/com/intercress/Semaphore.groovy
@@ -2,7 +2,7 @@
 package com.intercress
 import groovy.json.JsonOutput
 
-    myMethod() {
+    def myMethod() {
         httpRequest acceptType: 'APPLICATION_JSON', consoleLogResponseBody: true, contentType: 'APPLICATION_JSON', httpMode: 'POST', url: "http://localhost:3000/api/auth/login"
     }
            
