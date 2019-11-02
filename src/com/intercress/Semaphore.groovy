62da5c2239ad1261a0c4ce00a6026adabba3a997
timhaw <tim@intercress.com>
2019-11-02 22:11:44 +0000

    Update Semaphore.groovy

--- src/com/intercress/Semaphore.groovy
+++ src/com/intercress/Semaphore.groovy
@@ -4,7 +4,7 @@ import groovy.json.JsonOutput
 
 class Cookie {
     def void httpRequestCookie() {
-        httpRequest acceptType: 'APPLICATION_JSON', consoleLogResponseBody: true, contentType: 'APPLICATION_JSON', httpMode: 'POST', url: "http://localhost:3000/api/auth/login"
+//        httpRequest acceptType: 'APPLICATION_JSON', consoleLogResponseBody: true, contentType: 'APPLICATION_JSON', httpMode: 'POST', url: "http://localhost:3000/api/auth/login"
     }
 }
 
