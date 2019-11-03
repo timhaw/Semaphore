f160067fd44b7d747f46f03fc36a677db30ce404
timhaw <tim@intercress.com>
2019-11-03 20:20:02 +0000

    Update playbook.groovy

--- vars/playbook.groovy
+++ vars/playbook.groovy
@@ -78,7 +78,7 @@ def call(String playbook) {
         stage ('project') {
 //            projects = httpGetProjects(cookie)
             projects = '{"name":"katone","age":5}'
-//            jsonText = readJSON text: projects
+            jsonText = readJSON text: projects
 //            project = jsonText.find { it.value.name == 'Ansible' }
 //            project = jsonText.find { it.value.name == 'katone' }
 //            id = project.value.id
@@ -92,6 +92,6 @@ def call(String playbook) {
             status = httpSendTask(playbook, cookie)
         }
     
-        echo "Hello, ${projects}"
+        echo "Hello, ${jsonText}"
     }
 }
