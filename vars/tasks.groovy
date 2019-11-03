e23c0967589dac1b169f1d96d4e09bbc36a00660
timhaw <tim@intercress.com>
2019-11-03 07:53:26 +0000

    Update tasks.groovy

--- vars/tasks.groovy
+++ vars/tasks.groovy
@@ -1,15 +1,23 @@
 import com.intercress.*
+import groovy.json.JsonOutput
 
+String myMethod() {
+    def credentials = JsonOutput.toJson([auth: 'admin', password: 'admin'])
+    def cookie = httpRequest acceptType: 'APPLICATION_JSON', consoleLogResponseBody: true, contentType: 'APPLICATION_JSON', httpMode: 'POST', requestBody: credentials, url: "http://localhost:3000/api/auth/login"
+    def cookieContent = cookie.headers.get("Set-Cookie")    
+    return cookieContent
+}
+  
 def call(String playbook) {
   
-    def common = load "./httpRequestCookie"
-    //  def wibble = Semaphore.foo
-    def wibble = new Semaphore()
   
-    // retval = wibble.Semapi(playbook)
-    retval = common.httpRequestCookie()
+  //  def wibble = Semaphore.foo
+  def wibble = new Semaphore()
   
-    echo "Hello, ${retval}"
+  // retval = wibble.Semapi(playbook)
+  retval = myMethod()
+  
+  echo "Hello, ${retval}"
 }
 
 //def call(String playbook = 'local.yml') {
