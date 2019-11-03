3d7988b285a81c2a9607f9ec4e0a199b92a04c54
timhaw <tim@intercress.com>
2019-11-03 20:57:48 +0000

    Update playbook.groovy

--- vars/playbook.groovy
+++ vars/playbook.groovy
@@ -81,7 +81,7 @@ def call(String playbook) {
             jsonText = readJSON text: projects
 //            project = jsonText.find { it.value.name == 'Ansible' }
 //            project = jsonText.find { it.value.name == 'katone' }     // No such property: name for class: java.lang.String
-            project = jsonText.find { key, value -> key == 'playbook' }
+            project = jsonText.find { it.value.playbook == 'local.yml' }
 //            id = project.value.id
         }
     
