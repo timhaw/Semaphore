3dc7bba047d50fab195cd86c34bfbf54656d2a30
timhaw <tim@intercress.com>
2019-11-03 07:10:08 +0000

    Update httpRequestCookie.groovy

--- vars/httpRequestCookie.groovy
+++ vars/httpRequestCookie.groovy
@@ -1,8 +1,11 @@
+package httpRequestCookie
 import groovy.json.JsonOutput
 
-def String httpRequestCookie() {
-    def credentials = JsonOutput.toJson([auth: 'admin', password: 'admin'])
-    def cookie = httpRequest acceptType: 'APPLICATION_JSON', consoleLogResponseBody: true, contentType: 'APPLICATION_JSON', httpMode: 'POST', requestBody: credentials, url: "http://localhost:3000/api/auth/login"
-    def cookieContent = cookie.headers.get("Set-Cookie")    
-    return cookieContent
+class httpRequestCookie() {
+    def String request() {
+        def credentials = JsonOutput.toJson([auth: 'admin', password: 'admin'])
+        def cookie = httpRequest acceptType: 'APPLICATION_JSON', consoleLogResponseBody: true, contentType: 'APPLICATION_JSON', httpMode: 'POST', requestBody: credentials, url: "http://localhost:3000/api/auth/login"
+        def cookieContent = cookie.headers.get("Set-Cookie")    
+        return cookieContent
+    }
 }
