8d295c07c05e2ee1934c99e4d41d4d5658200d9b
timhaw <tim@intercress.com>
2019-11-04 13:43:23 +0000

    Update Semaphore.groovy

--- src/com/intercress/Semaphore.groovy
+++ src/com/intercress/Semaphore.groovy
@@ -5,25 +5,168 @@ import org.json
 // class HelloWorld implements Serializable {
 class Semaphore {
        
-    def getRequestParameterMap(def request) {
-        def params = request.getParameterNames()
-        def requestParamMap = [:]
-        while (params.hasMoreElements()) {
-            String fieldName = (String) params.nextElement();
-            if (fieldName.startsWith("invoice_")) {
-                JSONObject obj = JSONUtil.parseJSONObject(request.getParameter(fieldName));
-                def id = obj.get("id").toString()
-                def parcelId = obj.get("parcelId").toString()
-                def referenceId = obj.get("referenceId").toString()
-                requestParamMap["id"] = id
-                requestParamMap["parcelId"] = parcelId
-                requestParamMap["referenceId"] = referenceId
+   /**
+    * Get the nearest object or array end
+    */
+    def getNearestEnd(String json, int start, String head, String tail) {
+        def end = start
+        def count = 1
+        while (count > 0) {
+            end++
+            def c = json.charAt(end)
+            if (c == head) {
+                count++
+            } else if (c == tail) {
+                count--
             }
         }
-        requestParamMap
+        return end;
     }
-    
-    static String foo = 'bar'
+
+    /**
+     * Parse the object
+     */
+    def parseObject(String json) {
+        def map = [:]
+        def length = json.length()
+        def index = 1
+        def state = 'none' // none, string-value, other-value
+        def key = ''
+        while (index < length -1) {
+            def c = json.charAt(index)
+            switch(c) {
+                case '"':
+                    if (state == 'none') {
+                        def keyStart = index + 1;
+                        def keyEnd = keyStart;
+                        while (json.charAt(keyEnd) != '"') {
+                            keyEnd++
+                        }
+                        index = keyEnd
+                        def keyValue = json[keyStart .. keyEnd - 1]
+                        key = keyValue
+                    } else if (state == 'value') {
+                        def stringStart = index + 1;
+                        def stringEnd = stringStart;
+                        while (json.charAt(stringEnd) != '"') {
+                            stringEnd++
+                        }
+                        index = stringEnd
+                        def stringValue = json[stringStart .. stringEnd - 1]
+                        map.put(key, stringValue)
+                    }
+                    break
+
+            case '{':
+                def objectStart = index
+                def objectEnd = getNearestEnd json, index, '{', '}'
+                def objectValue = json[objectStart .. objectEnd]
+                map.put(key, parseObject(objectValue))
+                index = objectEnd
+                break
+
+            case '[':
+                def arrayStart = index
+                def arrayEnd = getNearestEnd(json, index, '[', ']')
+                def arrayValue = json[arrayStart .. arrayEnd]
+                map.put(key, parseArray(arrayValue))
+                index = arrayEnd
+                break
+
+            case ':':
+                state = 'value'
+                break
+
+            case ',':
+                state = 'none'
+                key = ''
+                break;
+
+            case ["\n", "\t", "\r", " "]:
+                break
+
+            default:
+                break
+        }
+        index++
+    }
+
+    return map
+}
+
+/**
+ * Parse the array
+ */
+def parseArray(String json) {
+    def list = []
+    def length = json.length()
+    def index = 1
+    def state = 'none' // none, string-value, other-value
+    while (index < length -1) {
+        def c = json.charAt(index)
+        switch(c) {
+            case '"':
+                def stringStart = index + 1;
+                def stringEnd = stringStart;
+                while (json.charAt(stringEnd) != '"') {
+                    stringEnd++
+                }
+                def stringValue = json[stringStart .. stringEnd - 1]
+                list.add(stringValue)
+                index = stringEnd
+                break
+
+            case '{':
+                def objectStart = index
+                def objectEnd = getNearestEnd(json, index, '{', '}')
+                def objectValue = json[objectStart .. objectEnd]
+                list.add(parseObject(objectValue))
+                index = objectEnd
+                break
+
+            case '[':
+                def arrayStart = index
+                def arrayEnd = getNearestEnd(json, index, '[', ']')
+                def arrayValue = json[arrayStart .. arrayEnd]
+                list.add(parseArray(arrayValue))
+                index = arrayEnd
+                break
+
+            case ["\n", "\t", "\r", " "]:
+                break
+
+            case ',':
+                state = 'none'
+                key = ''
+                break;
+
+            default:
+                break
+        }
+        index++
+    }
+
+    return list
+}
+
+/**
+ * Parse the JSON, object or array
+ */
+def parseJson(String json) {
+    def start = json[0]
+    if (start == '[') {
+        return parseArray(json)
+    } else if (start == '{') {
+        return parseObject(json)
+    } else {
+        return null
+    }
+}
+
+// Test code
+def project = parseJson('{"abdef":["Jim","Tom","Sam",["XYZ","ABC"]],{"namek":["adbc","cdef"]}}')
+       
+       static String foo = 'bar'
        
     static String FindProject(String projects, String playbook) {
            
@@ -33,9 +176,7 @@ class Semaphore {
 
 //        [{"id":1,"name":"Ansible","created":"2019-10-29T17:03:53Z","alert":false,"alert_chat":""},{"id":2,"name":"Test","created":"2019-11-03T10:20:26Z","alert":false,"alert_chat":""}]
         def _projects = [1: [id:1,name:'Ansible',created:'2019-10-29T17:03:53Z',alert:false,alert_chat:'']]
-           
-        def project = getRequestParameterMap(projects)
-           
+                      
            //        def project = _projects.find { it.value.name == 'Ansible' }
 //        def id = project.value.id
            
@@ -46,6 +187,7 @@ class Semaphore {
 //            assertTrue(jsonText.find { it.value == "local.yml"}.key == "playbook")
 //            project = jsonText.find { it.key == 'playbook' }
 //            id = project.value.id
-        return projects
+           
+        return project
     }
 }
