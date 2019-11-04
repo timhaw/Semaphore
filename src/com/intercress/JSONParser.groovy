432519805f124b5af14d4ea9f09e1e0b2f5ab071
timhaw <tim@intercress.com>
2019-11-04 17:09:25 +0000

    Update JSONParser.groovy

--- src/com/intercress/JSONParser.groovy
+++ src/com/intercress/JSONParser.groovy
@@ -3,6 +3,7 @@ package com.intercress
 import groovy.json.JsonSlurper
 
 class JSONParser {
-    static String jsonSlurper = new JsonSlurper()
-    return jsonSlurper().parseText(String text)
+    def jsonSlurper = new JsonSlurper()
+    static String parsedJson = jsonSlurper().parseText(String text)
+    return parsedJson
 }
