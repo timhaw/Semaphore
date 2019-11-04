4d6ae3bec8d64c2bc858393845f232eb7c6f59ef
timhaw <tim@intercress.com>
2019-11-04 09:16:55 +0000

    Update Semaphore.groovy

--- src/com/intercress/Semaphore.groovy
+++ src/com/intercress/Semaphore.groovy
@@ -8,12 +8,12 @@ class Semaphore implements Serializable {
        
     static String FindProject(String projects, String playbook) {
 //        def String parsedJson = readJSON text: '{"id":1,"name":"Ansible","created":"2019-10-29T17:03:53Z","alert":false,"alert_chat":""}'
-//            project = jsonText.find { it.value.name == 'Ansible' }
+            project = jsonText.find { it.value.playbook == 'local.yml' }
 //            project = jsonText.find { it.value.name == 'katone' }     // No such property: name for class: java.lang.String
 //            assertTrue(map.find{it.value == "New York"}.key == "city")
 //            assertTrue(jsonText.find { it.value == "local.yml"}.key == "playbook")
 //            project = jsonText.find { it.key == 'playbook' }
 //            id = project.value.id
-        return 'Hello World'
+        return 'project'
     }
 }
