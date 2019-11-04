6941a7024ea487b9af7b769294a9f8b1ec86e756
timhaw <tim@intercress.com>
2019-11-04 08:51:53 +0000

    Update Semaphore.groovy

--- src/com/intercress/Semaphore.groovy
+++ src/com/intercress/Semaphore.groovy
@@ -4,5 +4,7 @@ package com.intercress
 class Semaphore {
        
     static String foo = 'bar'
-                  
+       
+    static String FindProject(String projects, String playbook) {
+    return playbook
 }
