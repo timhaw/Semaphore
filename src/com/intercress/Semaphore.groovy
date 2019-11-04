f160ffff7782b59234d4b02bb46523c9fc25c57f
timhaw <tim@intercress.com>
2019-11-04 08:46:21 +0000

    Update Semaphore.groovy

--- src/com/intercress/Semaphore.groovy
+++ src/com/intercress/Semaphore.groovy
@@ -1,70 +1,8 @@
 #!/usr/bin/env groovy
 package com.intercress
-// import groovyx.net.http.HTTPBuilder
-// import static groovyx.net.http.ContentType.URLENC
-import groovy.json.JsonOutput
 
 class Semaphore {
        
     static String foo = 'bar'
                   
-    static String Semapi(String[] password) {
-        def credentials = JsonOutput.toJson([auth: 'admin', password: 'admin'])
-           
-//        def http = new HTTPBuilder( 'http://localhost:3000/api/auth/login/' )
-//        def postBody = [name: 'bob', title: 'construction worker'] // will be url-encoded
- 
-//        http.post( path: '/', body: postBody,
-//           requestContentType: URLENC ) { resp ->
-           
-//       httpRequestCookie()
-//       httpRequestCookie(credentials)
-               
-//       def cookie = httpRequest \
-//           acceptType: 'APPLICATION_JSON', \
-//           consoleLogResponseBody: true, \
-//           contentType: 'APPLICATION_JSON', \
-//           httpMode: 'POST', \
-//           requestBody: credentials, \
-//           url: "http://localhost:3000/api/auth/login"
-
-//        def cookieContent = cookie.headers.get("Set-Cookie")
-       
-        def cookieContent = credentials
-       
-        return cookieContent
-    }
-   
-   //    def schema = JsonOutput.toJson([template_id: 1, debug: false, dry_run: false, playbook: playbook, environment: ''])   
-   // refer to this in a pipeline using:
-   //
-   // import com.intercress.GlobalVars
-   // println GlobalVars.foo
 }
-
-//class Semaphore {
-//    String Semapi(String playbook) {
-        
-//       def credentials = JsonOutput.toJson([auth: 'admin', password: 'admin'])    
-//       def schema = JsonOutput.toJson([template_id: 1, debug: false, dry_run: false, playbook: playbook, environment: ''])   
-
-//        def cookie = httpRequest( \
-//            acceptType: 'APPLICATION_JSON', \
-//            consoleLogResponseBody: true, \
-//            contentType: 'APPLICATION_JSON', \
-//            httpMode: 'POST', \
-//            requestBody: credentials, \
-//            url: "http://localhost:3000/api/auth/login")
-
-//        def cookieContent = cookie.headers.get("Set-Cookie")    
-
-//        httpRequest( \
-//            acceptType: 'APPLICATION_JSON', \
-//            consoleLogResponseBody: true, \
-//            contentType: 'APPLICATION_JSON', \
-//            customHeaders: [[name: 'Cookie', value: cookieContent[0]]], \
-//            httpMode: 'POST', \
-//            requestBody: schema, \
-//            url: "http://localhost:3000/api/project/1/tasks")
-//    }
-//}
