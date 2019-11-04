e51e38420fa1d1daf79d708a14badccdee0ac129
timhaw <tim@intercress.com>
2019-11-04 09:00:30 +0000

    Update Semaphore.groovy

--- src/com/intercress/Semaphore.groovy
+++ src/com/intercress/Semaphore.groovy
@@ -6,12 +6,13 @@ class Semaphore {
     static String foo = 'bar'
        
     static String FindProject(String projects, String playbook) {
+        parsedJson = readJSON text: projects
 //            project = jsonText.find { it.value.name == 'Ansible' }
 //            project = jsonText.find { it.value.name == 'katone' }     // No such property: name for class: java.lang.String
 //            assertTrue(map.find{it.value == "New York"}.key == "city")
 //            assertTrue(jsonText.find { it.value == "local.yml"}.key == "playbook")
 //            project = jsonText.find { it.key == 'playbook' }
 //            id = project.value.id
-        return playbook
+        return parsedJson
     }
 }
