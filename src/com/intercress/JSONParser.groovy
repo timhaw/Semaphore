b6c871a99bb8ba614c81cbdde0a7d4726772a5e0
timhaw <tim@intercress.com>
2019-11-04 22:06:42 +0000

    Update JSONParser.groovy

--- src/com/intercress/JSONParser.groovy
+++ src/com/intercress/JSONParser.groovy
@@ -14,7 +14,8 @@ class JSONParser {
     static parseTemplates(String templates, String project, String name) {
         def slurper = new JsonSlurper()
         def parsedText = slurper.parseText(templates)
-        def projects = parsedText.findAll { it.project_id == project }
+        def projects = parsedText.findAll { it.project_id == '1' }
+//        def projects = parsedText.findAll { it.project_id == project }
 //        def String template = parsedText.find { it.id == project }.id
 //        return template
         return projects
