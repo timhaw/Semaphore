bd077a8659578ccf583fcced2e577e822cb96aca
timhaw <tim@intercress.com>
2019-11-04 20:31:57 +0000

    Update JSONParser.groovy

--- src/com/intercress/JSONParser.groovy
+++ src/com/intercress/JSONParser.groovy
@@ -3,11 +3,10 @@ package com.intercress
 import groovy.json.JsonSlurper
 
 class JSONParser {
-    static parseJson(String jsonText) {
+    static parseJson(String jsonText, String project) {
         def slurper = new JsonSlurper()
-//        def String jsonText = '[{"id":1,"name":"Ansible","created":"2019-10-29T17:03:53Z","alert":false,"alert_chat":""},{"id":2,"name":"Test","created":"2019-11-03T10:20:26Z","alert":false,"alert_chat":""}]'
         def parsedText = slurper.parseText(jsonText)
-        def String project = parsedText.find { it.name == 'Test' }.id
-        return project
+        def String id = parsedText.find { it.name == project }.id
+        return id
     }
 }
