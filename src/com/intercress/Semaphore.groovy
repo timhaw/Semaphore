c56055782bee238689ef842459493bb5f2b9c2e8
timhaw <tim@intercress.com>
2019-11-04 21:15:26 +0000

    Update Semaphore.groovy

--- src/com/intercress/Semaphore.groovy
+++ src/com/intercress/Semaphore.groovy
@@ -4,12 +4,12 @@ package com.intercress
 class Semaphore {
                   
     static String FindProject(String projects, String name) {
-        def String project = new JSONParser().parseJson(projects, name)
+        def String project = new JSONParser().parseProjects(projects, name)
         return project
     }
 
     static String FindTemplate(String templates, String id) {
-        def String template = new JSONParser().parseJson(templates, id)
+        def String template = new JSONParser().parseTemplates(templates, id)
         return template
     }
 }
