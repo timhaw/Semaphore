1394f66f2c2b4f9a0a9463b82516191b6778113f
timhaw <tim@intercress.com>
2019-11-04 09:33:23 +0000

    Update Semaphore.groovy

--- src/com/intercress/Semaphore.groovy
+++ src/com/intercress/Semaphore.groovy
@@ -9,10 +9,10 @@ class Semaphore implements Serializable {
     static String FindProject(String projects, String playbook) {
            
         def people = [
-            1: [name:'Bob', age: 32, gender: 'M'],
-            2: [name:'Johnny', age: 36, gender: 'M'],
-            3: [name:'Claire', age: 21, gender: 'F'],
-            4: [name:'Amy', age: 54, gender:'F']
+            [name:'Bob', age: 32, gender: 'M'],
+            [name:'Johnny', age: 36, gender: 'M'],
+            [name:'Claire', age: 21, gender: 'F'],
+            [name:'Amy', age: 54, gender:'F']
         ]
 
         def bob = people.find { it.value.name == 'Bob' } // find a single entry
