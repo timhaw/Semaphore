78389b7a3046d26611166b4754d3238babeed648
timhaw <tim@intercress.com>
2019-11-04 10:15:51 +0000

    Update Semaphore.groovy

--- src/com/intercress/Semaphore.groovy
+++ src/com/intercress/Semaphore.groovy
@@ -9,20 +9,21 @@ class Semaphore implements Serializable {
     static String FindProject(String projects, String playbook) {
            
         def people = [1: [name:'Bob', age: 32, gender: 'M'], 2: [name:'Johnny', age: 36, gender: 'M'], 3: [name:'Claire', age: 21, gender: 'F'], 4: [name:'Amy', age: 54, gender:'F']]
-
         def Claire = people.find { it.value.name == 'Claire' } // find a single entry
         def ageOfClaire = Claire.value.age
+
+        def String _projects = [1: [id:1,name:'Ansible',created:'2019-10-29T17:03:53Z',alert:false,alert_chat:'']]
+        def String  project = _projects.find { it.value.name == 'Ansible' }
+
 
            
 //        def String parsedJson = readJSON text: '{"id":1,"name":"Ansible","created":"2019-10-29T17:03:53Z","alert":false,"alert_chat":""}'
 //        def String  project = projects.find { it.value.name == 'Ansible' }
-//        def String _projects = [id:1,name:'Ansible',created:'2019-10-29T17:03:53Z',alert:false,alert_chat:'']
-//        def String  project = _projects.find { it.value.name == 'Ansible' }
 //            project = jsonText.find { it.value.name == 'katone' }     // No such property: name for class: java.lang.String
 //            assertTrue(map.find{it.value == "New York"}.key == "city")
 //            assertTrue(jsonText.find { it.value == "local.yml"}.key == "playbook")
 //            project = jsonText.find { it.key == 'playbook' }
 //            id = project.value.id
-        return ageOfClaire
+        return project
     }
 }
