a26e80f9e664306e3ce7274aef7402e6dca916eb
timhaw <tim@intercress.com>
2019-11-04 20:11:51 +0000

    Update JSONParser.groovy

--- src/com/intercress/JSONParser.groovy
+++ src/com/intercress/JSONParser.groovy
@@ -13,6 +13,6 @@ class JSONParser {
         
 //        assert slurper.name == 'Ansible'
 //        return slurper.parseText('{"id":63,"template_id":1,"status":"waiting","debug":false,"dry_run":false,"playbook":"local.yml","environment":"","user_id":1,"created":"2019-11-04T17:42:19.313545224Z","start":null,"end":null}')
-        return result
+        return project
     }
 }
