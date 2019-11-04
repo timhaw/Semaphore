13d8ca0a2cf5339b409ae21b2d3d097484af3b4c
timhaw <tim@intercress.com>
2019-11-04 20:01:38 +0000

    Update JSONParser.groovy

--- src/com/intercress/JSONParser.groovy
+++ src/com/intercress/JSONParser.groovy
@@ -8,7 +8,6 @@ class JSONParser {
 //        return slurper.parseText('{"person":{"name":"Guillaume","age":33,"pets":["dog","cat"]}}')
         def String jsonText = '[{"id":1,"name":"Ansible","created":"2019-10-29T17:03:53Z","alert":false,"alert_chat":""},{"id":2,"name":"Test","created":"2019-11-03T10:20:26Z","alert":false,"alert_chat":""}]'
         def parsedText = slurper.parseText(jsonText)
-        assert parsedText.name == 'Ansible'
         def names = parsedText.id + parsedText.name
 //        assert slurper.name == 'Ansible'
 //        def String project = parsedText.find { it.value.name == 'Ansible' }
