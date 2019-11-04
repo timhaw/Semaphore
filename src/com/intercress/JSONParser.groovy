9189aa29b5555299f418ea1f1d25990c57724abc
timhaw <tim@intercress.com>
2019-11-04 20:15:40 +0000

    Update JSONParser.groovy

--- src/com/intercress/JSONParser.groovy
+++ src/com/intercress/JSONParser.groovy
@@ -9,10 +9,10 @@ class JSONParser {
         def String jsonText = '[{"id":1,"name":"Ansible","created":"2019-10-29T17:03:53Z","alert":false,"alert_chat":""},{"id":2,"name":"Test","created":"2019-11-03T10:20:26Z","alert":false,"alert_chat":""}]'
         def parsedText = slurper.parseText(jsonText)
         def String project = parsedText.find { it.name == 'Test' }
-//        def result = project.value.id
+        def result = project.id
         
 //        assert slurper.name == 'Ansible'
 //        return slurper.parseText('{"id":63,"template_id":1,"status":"waiting","debug":false,"dry_run":false,"playbook":"local.yml","environment":"","user_id":1,"created":"2019-11-04T17:42:19.313545224Z","start":null,"end":null}')
-        return project
+        return result
     }
 }
