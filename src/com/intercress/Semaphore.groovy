cb4d2c2b8982ce1d77b109bcb3effb49b0d71208
timhaw <tim@intercress.com>
2019-11-04 21:05:57 +0000

    Update Semaphore.groovy

--- src/com/intercress/Semaphore.groovy
+++ src/com/intercress/Semaphore.groovy
@@ -3,9 +3,9 @@ package com.intercress
 
 class Semaphore {
                   
-    static String FindProject(String projects, String jobName) {
-        def String jsonText = '[{"id":1,"name":"Ansible","created":"2019-10-29T17:03:53Z","alert":false,"alert_chat":""},{"id":2,"name":"Test","created":"2019-11-03T10:20:26Z","alert":false,"alert_chat":""}]'
-        def String project = new JSONParser().parseJson(jsonText, jobName)
+    static String FindProject(String projects, String name) {
+//        def String jsonText = '[{"id":1,"name":"Ansible","created":"2019-10-29T17:03:53Z","alert":false,"alert_chat":""},{"id":2,"name":"Test","created":"2019-11-03T10:20:26Z","alert":false,"alert_chat":""}]'
+        def String project = new JSONParser().parseJson(projects, name)
         return project
     }
 }
