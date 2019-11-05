9fbba2dd1fdfef01e22e4e158fd0b8c9fffb4f43
timhaw <tim@intercress.com>
2019-11-05 07:29:08 +0000

    Update playbook.groovy

--- vars/playbook.groovy
+++ vars/playbook.groovy
@@ -84,7 +84,7 @@ def call(String project, String playbook) {
         }
     
         stage ('template') {
-//            templates = httpRequest Semaphore.getTemplates(cookie, project_id)
+            new_templates = httpRequest Semaphore.getTemplates(cookie, project_id)
             templates = httpGetTemplates(cookie, project_id)
             template_id = Semaphore.FindTemplate(templates, project_id, playbook)
         }
@@ -93,6 +93,6 @@ def call(String project, String playbook) {
             status = httpRequest Semaphore.sendTask(cookie, project_id, template_id, playbook)
         }
 
-        echo "Hello, ${templates}"
+        echo "Hello, ${new_templates}"
     }
 }
