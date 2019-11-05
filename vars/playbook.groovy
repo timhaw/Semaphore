29c7ca699b205147b95a9e483da10b21d333cb9d
timhaw <tim@intercress.com>
2019-11-05 08:10:05 +0000

    Update playbook.groovy

--- vars/playbook.groovy
+++ vars/playbook.groovy
@@ -75,7 +75,7 @@ def call(String project, String playbook) {
             credentials.passwordVariable = 'password'
             withCredentials([usernamePassword(credentials)]) {
                 cookie = httpRequestCookie(username, password)[0]
-                result = Semaphore.requestCookie(username, password)[0]
+                result = Semaphore.requestCookie(username, password)
 //                response = httpRequest Semaphore.requestCookie(username, password)[0]
 //                cookie = response.headers.get("Set-Cookie")    
             }
