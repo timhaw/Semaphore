60cdf14b89621a25bdca0b090c83008862510eef
timhaw <tim@intercress.com>
2019-11-05 06:23:57 +0000

    Update Semaphore.groovy

--- src/com/intercress/Semaphore.groovy
+++ src/com/intercress/Semaphore.groovy
@@ -1,8 +1,70 @@
 #!/usr/bin/env groovy
 package com.intercress
+import groovy.json.JsonOutput
 
-class Semaphore {
-                  
+class Semaphore {               
+
+    static String httpRequestCookie(String username, String password) {
+        def credentials = JsonOutput.toJson([auth: username, password: password])
+        def requestParams = [:]
+        requestParams.acceptType = 'APPLICATION_JSON'
+        requestParams.consoleLogResponseBody = true
+        requestParams.contentType = 'APPLICATION_JSON'
+        requestParams.httpMode = 'POST'
+        requestParams.requestBody = credentials
+        requestParams.url = 'http://localhost:3000/api/auth/login'
+        def cookie = httpRequest requestParams
+        def cookieContent = cookie.headers.get("Set-Cookie")    
+        return cookieContent
+    }
+
+    static String httpGetProjects(String cookie) {
+        def cookieHeader = [:]
+        cookieHeader.name = 'Cookie'
+        cookieHeader.value = cookie
+        def requestParams = [:]
+        requestParams.acceptType = 'APPLICATION_JSON'
+        requestParams.consoleLogResponseBody = true
+        requestParams.contentType = 'APPLICATION_JSON'
+        requestParams.customHeaders = [cookieHeader]
+        requestParams.httpMode = 'GET'
+        requestParams.url = 'http://localhost:3000/api/projects'
+        def response = httpRequest requestParams
+        return response.content
+    }
+
+    static String httpGetTemplates(String cookie, String project) {
+        def cookieHeader = [:]
+        cookieHeader.name = 'Cookie'
+        cookieHeader.value = cookie
+        def requestParams = [:]
+        requestParams.acceptType = 'APPLICATION_JSON'
+        requestParams.consoleLogResponseBody = true
+        requestParams.contentType = 'APPLICATION_JSON'
+        requestParams.customHeaders = [cookieHeader]
+        requestParams.httpMode = 'GET'
+        requestParams.url = "http://localhost:3000/api/project/${project}/templates?sort=alias&order=asc"
+        def response = httpRequest requestParams
+        return response.content
+    }
+
+    static String httpSendTask(String cookie, String project, String template, String playbook) {
+        def schema = JsonOutput.toJson([template_id: template.toInteger(), debug: false, dry_run: false, playbook: playbook, environment: ''])
+        def cookieHeader = [:]
+        cookieHeader.name = 'Cookie'
+        cookieHeader.value = cookie
+        def requestParams = [:]
+        requestParams.acceptType = 'APPLICATION_JSON'
+        requestParams.consoleLogResponseBody = true
+        requestParams.contentType = 'APPLICATION_JSON'
+        requestParams.customHeaders = [cookieHeader]
+        requestParams.httpMode = 'POST'
+        requestParams.requestBody = schema
+        requestParams.url = "http://localhost:3000/api/project/${project}/tasks"
+        def response = httpRequest requestParams
+        return response
+    }
+
     static String FindProject(String projects, String name) {
         def String id = new JSONParser().parseProjects(projects, name)
         return id
